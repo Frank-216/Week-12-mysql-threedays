eff744091c13298c4b2a85d1997a4eab302fa004
Frank Capraro <Frank@Franks-MBP.fios-router.home>
2016-07-20 22:00:11 -0400

    added the mysql module and began to update information from the git status

--- Bamazon.sql
+++ Bamazon.sql
@@ -12,3 +12,27 @@ CREATE TABLE Products(
 );
 
 SELECT * FROM Products;
+
+CREATE DATABASE Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE Products(
+	ItemID INT NOT NULL AUTO_INCREMENT,
+    ProductName VARCHAR(100) NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    StockQuantity INT(10) NOT NULL,
+    PRIMARY KEY(`ItemID`)
+);
+SELECT * FROM Products;
+
+INSERT INTO `Products` (`ProductName`, `DepartmentName`, `Price`,`StockQuantity`) VALUES ('Ice Cream', 'Dairy', '2.16','1000');
+INSERT INTO `Products` (`ProductName`, `DepartmentName`, `Price`,`StockQuantity`) VALUES ('X-Box', 'Gaming', '299.99','10000');
+INSERT INTO `Products` (`ProductName`, `DepartmentName`, `Price`,`StockQuantity`) VALUES ('Playstation 4', 'Gaming', '300.16','99999');
+INSERT INTO `Products` (`ProductName`, `DepartmentName`, `Price`,`StockQuantity`) VALUES ('Running Shoes', 'Footwear', '99.99','7777');
+INSERT INTO `Products` (`ProductName`, `DepartmentName`, `Price`,`StockQuantity`) VALUES ('Ice Cream', 'Dairy', '2.16','1000');
+
+
+
+
---
+++ BamazonCustomer.js
@@ -0,0 +1,35 @@
+// require mysql 
+var mysql = require('mysql');
+
+// set up connection 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root", //Your username
+    password: "", //Your password
+    database: "Bamazon"
+})
+// ensure that connection is running and display the port 
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+})
+// The function below can be used to enter values into the database.   
+// Items can also be added via the workbench
+
+// connection.query("INSERT INTO Products SET ?", {
+//     // Ask if I need to update the idem that is Auto Incremented
+//     ItemID: ,
+//     ProductName: "Alienware 15inch",
+//     DepartmentName:"Computers",
+//     Price:1399.99,
+//     StockQuantity:1000
+// }, function(err, res) {
+//     console.log(res);
+// });
+
+
+// query to display ID, Item, Price 
+connection.query('SELECT * FROM Products', function(err, res){
+    console.log(res);
+});
